5f67166fd32e60f351049df7ee50a77f050c1f89
Eric Berglund <esberglu@us.ibm.com>
2017-06-15 20:43:14 -0500

    Patch any devstack project
    This sets up the infrastructure to patch any openstack project by
    specifying pairs of repos and change number lists. This will allow
    much greater flexibility in the CI environment. When patches are added
    to a patching.conf file they will be applied the next CI run. We will
    now be able to apply workarounds and test patches with more control and
    less disruption.
    
    Change-Id: I9286735fd21a4b49f006c4a5f7d32edb9f6fb573

--- devstack/prep_devstack.sh
+++ devstack/prep_devstack.sh
@@ -46,18 +46,15 @@ get_latest_patch() {
     return 0
 }
 
-in_tree=false
-conf_file=outoftree.local.conf
+driver=outoftree
 FORCE=false
 pypowervm_patch_list=
 nova_patch_list=
-
 while getopts ":n:p:d:fh" opt; do
     case $opt in
         d)
             if [[ $OPTARG == 'in-tree' ]]; then
-                in_tree=true
-                conf_file=intree.local.conf
+                driver=intree
             elif [[ $OPTARG == 'out-of-tree' ]]; then
                 :
             else
@@ -106,7 +103,7 @@ fi
 # facilitate cleanup
 vm_id=`/opt/nodepool-scripts/my_vm_id.sh`
 sed "s/^instance_name_template =.*/instance_name_template = pvm$vm_id-%(display_name).11s-%(uuid).8s/" \
-    /opt/stack/powervm-ci/devstack/$ZUUL_BRANCH/$conf_file > /opt/stack/devstack/local.conf
+    /opt/stack/powervm-ci/devstack/$ZUUL_BRANCH/$driver/local.conf > /opt/stack/devstack/local.conf
 
 # Logs setup
 mkdir -p /opt/stack/logs
@@ -118,6 +115,28 @@ for proj in ceilometer ceilometer-powervm cinder devstack glance horizon keyston
     git checkout $ZUUL_BRANCH
     git pull
 done
+
+# For each line in the patching conf file, try to merge all patches in the list
+while read line; do
+    [[ -z $line ]] && continue
+    repo=$(echo $line | awk -F: '{print $1}')
+    change_list=$(echo $line | awk -F: '{print $2}')
+    repo_url=https://review.openstack.org/openstack/$repo/
+    cd /opt/stack/$repo
+    for change in $change_list; do
+        patch=`get_latest_patch "$repo_url" "$change"`
+        refspec=${patch#*|}
+        commit=${patch%|*}
+        # Only apply the patch if it's not in the chain of the change set we're testing.
+        if git log --pretty=format:%H {{ nova_chain_base_commit }}..HEAD | grep -q $commit; then
+            echo "Skipping $repo commit $commit of change $i because it's already in the commit chain."
+        else
+            echo "Applying $repo ref $refspec of change set $i (commit $commit)"
+            git fetch "$repo_url" "$refspec"
+            git cherry-pick --keep-redundant-commits FETCH_HEAD
+        fi
+    done
+done < /opt/stack/powervm-ci/devstack/$ZUUL_BRANCH/$driver/patching.conf
 
 # Tempest doesn't follow the same branching scheme, only has a remote master branch
 cd /opt/stack/tempest
@@ -136,24 +155,6 @@ if ! $FORCE || [ "$ZUUL_PROJECT""$BASE_LOG_PATH" ]; then
     cd /opt/stack/${ZUUL_PROJECT##*/}
     git fetch https://review.openstack.org/$ZUUL_PROJECT refs/changes/$BASE_LOG_PATH
     git checkout FETCH_HEAD
-fi
-
-if [ "$ZUUL_BRANCH" == "master" ]; then
-    OPENSTACK_REPO="https://review.openstack.org/openstack/nova"
-    cd /opt/stack/nova
-    for i in $(echo $nova_patch_list | sed "s/,/ /g"); do
-        patch=`get_latest_patch "$OPENSTACK_REPO" "$i"`
-        refspec=${patch#*|}
-        commit=${patch%|*}
-        # Only apply the patch if it's not in the chain of the change set we're testing.
-        if git log --pretty=format:%H {{ nova_chain_base_commit }}..HEAD | grep -q $commit; then
-            echo "Skipping nova commit $commit of change $i because it's already in the commit chain."
-        else
-            echo "Applying nova ref $refspec of change set $i (commit $commit)"
-            git fetch "$OPENSTACK_REPO" $refspec
-            git cherry-pick --keep-redundant-commits FETCH_HEAD
-        fi
-    done
 fi
 
 pypowervm_version=$(awk -F= '$1=="pypowervm" {print $NF}' /opt/stack/requirements/upper-constraints.txt)
@@ -251,7 +252,7 @@ if [ "$ZUUL_BRANCH" != "stable/ocata" ] && [ "$ZUUL_BRANCH" != "stable/newton" ]
 fi
 
 # Create public and private networks for the tempest runs
-if ! $in_tree; then
+if [ "$driver" != "intree" ]; then
     source /opt/stack/devstack/openrc admin admin
     #TODO: Devstack isn't respecting NEUTRON_CREATE_INITIAL_NETWORKS=False.
     # Deleting the created private network for now. Further investigation needed.
