21764f5143498edf126002f8baec527fd09f85e6
openstack-ci <esberglu@us.ibm.com>
2017-08-10 11:05:12 -0500

    Move process_logs to powervm-ci
    The process_logs.sh script was previously found in the internal
    neo-os-ci repo. By moving the script to powervm-ci, any further changes
    to the script will be picked up instantly by the next CI run. Stopping
    all devstack services and outputting the journalctl logs to files are
    also included in the process_logs script instead of being defined in the
    jenkins job configuration
    
    Change-Id: I9f6ba00cf27bd4f29ca43e8f59bd0d1011fc0141

---
+++ scripts/process_logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -xe
+
+# Copyright 2017, IBM Corp.
+#
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+find -L /opt/stack/logs -type l -delete
+sudo systemctl stop devstack@*
+for u in `sudo systemctl --no-legend --no-pager list-unit-files 'devstack@*' | awk -F. '{print $1}'`; do
+     sudo journalctl -a -o short-precise --unit $u > /opt/stack/logs/${u#*@}.txt
+done
+sed -i 's/9.\([0-9]\{1,3\}\.\)\{2\}[0-9]\{1,3\}/172.16.0.1/g; s/\([0-9a-zA-Z]*\).[0-9a-zA-Z.]*.ibm.com/\1.cleared.domain.name/g' /opt/stack/logs/*
+for f in /opt/stack/logs/*.log; do
+    actual_file=`realpath $f`
+    # The -f flag ensures that the file will overwrite any existing files with that name
+    gzip -f "$actual_file"
+done
