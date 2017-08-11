a87286cdabbc3da8785e6e63b09697ab4b429f4c
openstack-ci <esberglu@us.ibm.com>
2017-08-11 10:04:50 -0500

    Update prep_devstack for pike
    This updates the prep_devstack.sh script to support the pike branch.
    
    Change-Id: I2e4cfd02609ad2d9262ec5d3bb28a14cfa440855

--- devstack/prep_devstack.sh
+++ devstack/prep_devstack.sh
@@ -233,7 +233,7 @@ sudo ppc64_cpu --smt=on
 nova-manage cell_v2 discover_hosts
 
 source /opt/stack/devstack/openrc admin admin
-if [ "$ZUUL_BRANCH" == "master" ]; then
+if [ "$ZUUL_BRANCH" == "master" ] || [ "$ZUUL_BRANCH" == "stable/pike" ]; then
     # TODO: Remove once fix for https://bugs.launchpad.net/devstack/+bug/1699870 is released.
     # Devstack isn't respecting NEUTRON_CREATE_INITIAL_NETWORKS=False. For now we will delete the
     # network after stacking.
