c57234a4737b99d74ebbe484fb956f3c753955bb
openstack-ci <esberglu@us.ibm.com>
2017-08-14 12:21:19 -0500

    Move log scp task to external script
    This moves the scp task from the jenkins-jobs to the external scripts.
    This will allow us to instantly deploy changes to the script and
    should ease the transition to zuul v3. There is a corresponding change
    in neo-os-ci that passes the necessary vars to the script.
    
    Change-Id: I468ee46267ea6884370bac31bf1fddbe3b822793

--- scripts/process_logs.sh
+++ scripts/process_logs.sh
@@ -17,40 +17,57 @@
 # limitations under the License.
 
 zuul_branch=$1
-log_path=/opt/stack/logs
-find -L $log_path -type l -delete
+logserver_user=$2
+logserver_ip=$3
+
+# Base log path for logserver
+zuul_base_log_path=$4
+logserver_path=/srv/static/logs/$zuul_base_log_path
+
+# Path to logs on all-in-one test vms
+stack_log_path=/opt/stack/logs
+
+find -L $stack_log_path -type l -delete
 
 # Branches after ocata use journald for logging. These need to be output to files
 # to be copied to the logserver.
 if [ "$zuul_branch" != "stable/newton" ] && [ "$zuul_branch" != "stable/ocata" ]; then
     sudo systemctl stop devstack@*
     for u in `sudo systemctl --no-legend --no-pager list-unit-files 'devstack@*' | awk -F. '{print $1}'`; do
-        sudo journalctl -a -o short-precise --unit $u > $log_path/${u#*@}.txt
+        sudo journalctl -a -o short-precise --unit $u > $stack_log_path/${u#*@}.txt
     done
 fi
 
 # Rename any additional log files with .txt extension for in browser viewing.
-for f in $log_path/*.log; do
+for f in $stack_log_path/*.log; do
     mv -- "$f" "${f%.log}.txt"
 done
 
-# Copy apache logs to log_path so they get picked up by scp to logserver.
+# Copy apache logs to stack_log_path so they get picked up by scp to logserver.
 apache_logs=$(sudo ls /var/log/apache2/)
 for f in $apache_logs; do
    filename=$f
    if [[ "$f" != *.gz ]]; then
        filename=$f.txt
    fi
-   sudo cp /var/log/apache2/$f $log_path/$filename
-   sudo chown jenkins:jenkins $log_path/$filename
+   sudo cp /var/log/apache2/$f $stack_log_path/$filename
+   sudo chown jenkins:jenkins $stack_log_path/$filename
 done
 
 # Scrub IPs and domain names
-sed -i 's/9.\([0-9]\{1,3\}\.\)\{2\}[0-9]\{1,3\}/172.16.0.1/g; s/\([0-9a-zA-Z]*\)\.[0-9a-zA-Z.]*ibm.com/\1.cleared.domain.name/g' $log_path/*
+sed -i 's/9.\([0-9]\{1,3\}\.\)\{2\}[0-9]\{1,3\}/172.16.0.1/g; s/\([0-9a-zA-Z]*\)\.[0-9a-zA-Z.]*ibm.com/\1.cleared.domain.name/g' $stack_log_path/*
 
 # Zip all logs
-for f in $log_path/*.txt; do
+for f in $stack_log_path/*.txt; do
     actual_file=`realpath $f`
     # The -f flag ensures that the file will overwrite any existing files with that name
     gzip -f "$actual_file"
 done
+
+# Copy logs to logserver
+eval `ssh-agent -s`
+ssh-add /opt/nodepool-scripts/osci_rsa
+ssh-keyscan $logserver_ip >> ~/.ssh/known_hosts
+ssh $logserver_user@$logserver_ip "mkdir -p $logserver_path/logs"
+scp $stack_log_path/*.gz $logserver_user@$logserver_ip:$logserver_path/logs/
+scp $stack_log_path/*powervm_os_ci* $logserver_user@$logserver_ip:$logserver_path
