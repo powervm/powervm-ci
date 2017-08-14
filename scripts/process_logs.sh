547344760c2fbda0ffee22190b491920e277320d
openstack-ci <esberglu@us.ibm.com>
2017-08-13 20:45:49 -0500

    Process log fix for newton and ocata
    Newton and ocata stable branches use screen for logging as opposed to
    journald for newer branches. This updates the process_logs.sh script
    to handle the stable logs properly.
    
    Change-Id: Ib6373ad320a571255f755364fd994f61df0f9956

--- scripts/process_logs.sh
+++ scripts/process_logs.sh
@@ -15,14 +15,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+zuul_branch=$1
 log_path=/opt/stack/logs
 find -L $log_path -type l -delete
 sudo systemctl stop devstack@*
-for u in `sudo systemctl --no-legend --no-pager list-unit-files 'devstack@*' | awk -F. '{print $1}'`; do
-     sudo journalctl -a -o short-precise --unit $u > $log_path/${u#*@}.txt
-done
-mv $log_path/stack.sh.log $log_path/stack.sh.txt
-mv $log_path/tempest.log $log_path/tempest.txt
+if [ "$zuul_branch" == "stable/newton" ] || [ "$zuul_branch" == "stable/ocata" ]; then
+    for f in $log_path/*.log; do
+        mv -- "$f" "${f%.log}.txt"
+    done
+else
+    sudo systemctl stop devstack@*
+    for u in `sudo systemctl --no-legend --no-pager list-unit-files 'devstack@*' | awk -F. '{print $1}'`; do
+        sudo journalctl -a -o short-precise --unit $u > $log_path/${u#*@}.txt
+    done
+    mv $log_path/stack.sh.log $log_path/stack.sh.txt
+    mv $log_path/tempest.log $log_path/tempest.txt
+fi
 apache_logs=$(sudo ls /var/log/apache2/)
 for f in $apache_logs; do
    filename=$f
