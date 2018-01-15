07395e8a39477540104d161e10455ef016550b1b
esberglu <esberglu@us.ibm.com>
2018-01-15 16:04:35 -0600

    Refactor post_stack_vm_cleaner
    This updates the post_stack_vm_cleaner script to run the storage scrub
    task [1] instead of finding and deleting the storage elements. It also
    use the new power off method [2] and has some general cleanup.
    
    [1] https://github.com/powervm/pypowervm/blob/1.1.10/pypowervm/tasks/storage.py#L1165
    [2] https://github.com/powervm/pypowervm/commit/6ce4d5d31
    
    Change-Id: I62cf64b5e4b6a73807bceedfd62d4cf030afeaa4

--- scripts/post_stack_vm_cleaner.py
+++ scripts/post_stack_vm_cleaner.py
@@ -1,4 +1,4 @@
-# Copyright 2014, 2017 IBM Corp.
+# Copyright 2014, 2018 IBM Corp.
 #
 # All Rights Reserved.
 #
@@ -17,16 +17,17 @@
 import subprocess
 
 from pypowervm import adapter
+from pypowervm import const
 from pypowervm.tasks import power
+from pypowervm.tasks import power_opts
+from pypowervm.tasks import storage as tsk_stor
 from pypowervm import util as pvm_util
-from pypowervm.wrappers import base_partition as pvm_bp
+from pypowervm.utils import transaction as tx
 from pypowervm.wrappers import logical_partition as pvm_lpar
 from pypowervm.wrappers import managed_system as pvm_ms
-from pypowervm.wrappers import network as pvm_net
-from pypowervm.wrappers import storage as pvm_stor
 from pypowervm.wrappers import virtual_io_server as pvm_vios
 
-# This script runs post a devstack compute run.  Its purpose is to clean up
+# This script runs after a devstack compute run.  Its purpose is to clean up
 # any VMs that were left around from a tempest test.  Tempest may fail half
 # way through and leave a bunch of VMs laying around.
 #
@@ -38,71 +39,29 @@ from pypowervm.wrappers import virtual_io_server as pvm_vios
 # Builder (JJB), for both success and failure runs.
 
 
-def find_all_target_vms(adpt, host_uuid):
-    """Finds the peer VMs."""
+def find_target_vms(adpt):
+    """Find all vms created for tempest testing."""
     lpar_id = pvm_util.my_partition_id()
 
     # Get each LPAR and see if it was created for this process
     lpar_wraps = pvm_lpar.LPAR.get(adpt)
     actual_lpar_wraps = []
     for lpar_w in lpar_wraps:
+        # All instances created by tempest will start with this prefix as
+        # specified by the instance_name_template set in prep_devstack.sh
         if not lpar_w.name.startswith('pvm%s-tempest' % lpar_id):
             continue
-
-        if lpar_w.state in [pvm_bp.LPARState.RUNNING,
-                            pvm_bp.LPARState.OPEN_FIRMWARE]:
-            power.power_off(lpar_w, host_uuid, force_immediate=True)
         actual_lpar_wraps.append(lpar_w)
 
     return actual_lpar_wraps
 
 
-def find_and_remove_scsi_mappings(adpt, lpar_wraps):
-    """Finds and removes the scsi mappings off a VIOS for a set of LPARs."""
-    # Only one vios
-    vios_wrap = pvm_vios.VIOS.get(adpt,
-                                  xag=[pvm_vios.VIOS.xags.SCSI_MAPPING])[0]
-    to_del_ids = [lpar_w.id for lpar_w in lpar_wraps]
-
-    # Find the maps to delete
-    scsi_maps_to_del = []
-    for scsi_map in vios_wrap.scsi_mappings:
-         if scsi_map.client_adapter.lpar_id in to_del_ids:
-             scsi_maps_to_del.append(scsi_map)
-
-    # remove and update the VIOS
-    for map_to_del in scsi_maps_to_del:
-        vios_wrap.scsi_mappings.remove(map_to_del)
-    vios_wrap.update()
-
-    # Return the deleted mappings
-    return scsi_maps_to_del
-
-
-def remove_backing_storage(adpt, scsi_maps):
-    """Deletes the backing storage for the scsi maps."""
-    try:
-        # Find the SSPs and clean out
-        ssp_resp = adpt.read(pvm_stor.SSP.schema_type)
-        ssp_feed = pvm_stor.SSP.wrap(ssp_resp)
-        for ssp in ssp_feed:
-            print "Shared Storage Pool %s" % ssp.name
-            lu_to_del = []
-
-            # Find all the LU's to delete
-            for scsi_map in scsi_mappings:
-                for lu in ssp.logical_units:
-                    if scsi_map.backing_storage.udid == lu.udid:
-                        lu_to_del.append(lu)
-
-            # Now remove the LU's from the list
-            for lu in lu_to_del:
-                ssp.logical_units.remove(lu)
-            ssp.update()
-            print "Shared Storage Pool update complete"
-    except Exception as ssp_e:
-        # This can occur if other nodes in the cloud still use a disk.
-        print "Error with SSP Cleaning.  Non-blocking."
+def scrub_storage(adpt, lpar_wraps):
+    ftsk = tx.FeedTask(
+        'scrub_stg', pvm_vios.VIOS.getter(adpt, xag=[const.XAG.VIO_SMAP]))
+    tsk_stor.add_lpar_storage_scrub_tasks(
+        [lwrap.id for lwrap in lpar_wraps], ftsk, lpars_exist=False)
+    ftsk.execute()
 
 
 def delete_lpars(adpt, lpar_wraps):
@@ -115,20 +74,23 @@ def delete_lpars(adpt, lpar_wraps):
 
 def main():
     try:
-        adpt = adapter.Adapter(adapter.Session())
-        ms = pvm_ms.System.wrap(adpt.read(pvm_ms.System.schema_type))[0]
+        adpt = adapter.Adapter()
+        ms = pvm_ms.System.get(adpt)[0]
 
-        # Find the VMs that are paired to this system.
-        lpar_wraps = find_all_target_vms(adpt, ms.uuid)
+        # Find the tempest VMs that are paired to this system.
+        lpar_wraps = find_target_vms(adpt)
 
-        # Find and remove the SCSI mappings
-        scsi_mappings = find_and_remove_scsi_mappings(adpt, lpar_wraps)
+        # Power off all lpars
+        for lpar_w in lpar_wraps:
+            power.PowerOp.stop(
+                lpar_w, opts=power_opts.PowerOffOpts().vsp_hard())
 
-        # Delete the backing storage
-        remove_backing_storage(adpt, scsi_mappings)
-
-        # And end with a delete of the VMs
+        # Delete the VMs. This has to happen before the storage scrub.
         delete_lpars(adpt, lpar_wraps)
+
+        # Remove stale vSCSI mappings and backing storage.
+        scrub_storage(adpt, lpar_wraps)
+
     except Exception as e:
         print str(e)
         print "Error during clean up.  Non-blocking"
